e8fe079c2cb9d93ea3ca4769a3ea7582893d91c2
le_hung <le.hung@rivercrane.vn>
2023-08-09 23:07:30 +0700

    change header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <header class="header ">
-    <div class="wrapper">
-      <div class="header-main">
-        <div class="menu-link d-flex justify-content-space-between">
-          <ul class="menu-list">
+  <header class="header">
+    <div class="header_main">
+      <div class="wrapper">
+        <div class="menu-link d-flex justify-content-space-between ml-1 mr-1">
+          <ul class="menu-inline menu-list">
             <li class="menu-item" v-for="(item, index) in menu" :key="index">
-              <a :href="item.href" :class="'menu-link '+ item.class" v-bind="item.class">{{ item.title }}</a>
+              <a :href="item.href" :class="menu-link">{{ item.title }}</a>
             </li>
           </ul>
-          <div class="menu-user">
+          <div class="menu-inline menu-user">
             <li class="menu-item">
               <a href="#work" class="menu-link">Service</a>
             </li>
@@ -40,9 +40,18 @@ export default {
   data() {
     return {
       menu: [
-        { title: "John Doe", href: "john@example.com", class: '' },
-        { title: "Jane Smith", href: "jane@example.com" },
-        { title: "Michael Johnson", href: "michael@example.com" },
+        { title: "総合トップ", href: "/",},
+        { title: "ショッピング", href: "/" },
+        { title: "中古パーツ", href: "/" },
+        { title: "バイク選び", href: "/" },
+        { title: "ショップNAVI", href: "/" },
+        { title: "メディア総合", href: "/" },
+        { title: "サービス", href: "/" },
+        { title: "コミュニティ", href: "/" },
+        { title: "イベントNAVI", href: "/" },
+        { title: "レース", href: "/" },
+        { title: "EWC", href: "/" },
+        { title: "ご利用案内", href: "/" },
         // Thêm dữ liệu giả khác ở đây
       ],
     };
@@ -72,6 +81,42 @@ $justify-content: center, start, end, flex-start, flex-end, space-between
   .justify-content-#{$var}
     justify-content: $var
 
+$position: top, right, bottom, left
+
+@for $var from 0 through 5
+  .m-#{$var}
+    margin: 10px*$var
+  .p-#{$var}
+    padding: 10px*$var
+    
+  @each $p in $position
+    .m#{str-slice($p, 0, 1)}-#{$var}
+      margin-#{$p}: 10px*$var
+    .p#{str-slice($p, 0, 1)}-#{$var}
+      padding-#{$p}: 10px*$var
+
+header.header
+  $spacing: 10px
+  $black-blue: #2a313c
+  $red: #e61e25
+  .header_main
+    background: $black-blue
+    border-bottom: 3px solid $red
+    .menu-inline
+      display: block
+      >li
+        display: inline-block
+        transition: .325s
+        &:hover
+          background: $red
+        &.current
+          background: $red
+    .menu-item
+      >a 
+        color: $white
+        line-height: 1.5
+        display: block
+        padding: $spacing*.8
 
 // #app {
 //   font-family: Avenir, Helvetica, Arial, sans-serif;
